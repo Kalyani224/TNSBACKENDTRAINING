4586260372fca8f11d65d1698b911242614d8c25
Naveen kumar <Naveen kumar@DESKTOP-5E3L5TA>
2025-08-14 20:17:57 +0530

    static keyword

---
+++ TNS/src/Student.java
@@ -0,0 +1,17 @@
+//Demo for Static Keyword
+public class Student {
+	int rollno;
+	String sname;
+	String sbranch;
+	static String collegename="KPRIT";
+	void print() {
+		System.out.println("The details of the student are "
+				+rollno+" "
+				+sname+" "
+				+sbranch+" "
+				+collegename);
+	}
+	
+	
+
+}
---
+++ TNS/src/StudentMain.java
@@ -0,0 +1,12 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student ob= new Student();
+		ob.rollno=6634;
+		ob.sname="kalyani";
+		ob.sbranch="AIML";
+		ob.print();
+	}
+
+}
