0d612ebcc84f0ac931bedc89b710819af7ef7f59
Naveen kumar <Naveen kumar@DESKTOP-5E3L5TA>
2025-08-14 20:05:33 +0530

    type casting

---
+++ TNS/src/ImplicitCasting.java
@@ -0,0 +1,12 @@
+
+public class ImplicitCasting {
+
+	public static void main(String[] args) {
+		int x = 10;
+        double y = x; 
+        System.out.println("Integer value: " + x);
+        System.out.println("Double value: " + y);
+
+	}
+
+}
