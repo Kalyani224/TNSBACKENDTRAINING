75eac360832c3172b6d7e48c40b8994ff4605746
Naveen kumar <Naveen kumar@DESKTOP-5E3L5TA>
2025-08-20 14:21:39 +0530

    operators

---
+++ TNS/src/ArithmeticOperators.java
@@ -0,0 +1,16 @@
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		
+        int num1 = 50;
+        int num2 = 15;
+        System.out.println("Addition: " + (num1 + num2));       
+        System.out.println("Subtraction: " + (num1 - num2));   
+        System.out.println("Multiplication: " + (num1 * num2)); 
+        System.out.println("Division: " + (num1 / num2));       
+        System.out.println("Modulus: " + (num1 % num2));      
+   
+	}
+
+}
---
+++ TNS/src/ShiftOperators.java
@@ -0,0 +1,15 @@
+
+public class ShiftOperators {
+
+	public static void main(String[] args) {
+		int number1 = 12, number2 = 25, result;
+
+	    // bitwise OR between 12 and 25
+	    result = number1 | number2;
+	    System.out.println(result);    // prints 29
+	 // bitwise AND between 12 and 25
+	    result = number1 & number2;
+	    System.out.println(result);    // prints 8
+	}
+
+}
