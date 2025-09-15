0edce1b88eb81cc4e0a99211e1b4bd3ee8376284
Naveen kumar <Naveen kumar@DESKTOP-5E3L5TA>
2025-09-15 22:30:59 +0530

    array

---
+++ TNS/src/com/kprit/tns/arraysdemo/ReverseArray.java
@@ -0,0 +1,15 @@
+package com.kprit.tns.arraysdemo;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		
+		 int[] arr = {98, 34, 76, 23, 678};
+		 
+	     for (int i = arr.length - 1; i >= 0; i--) {
+	         System.out.print(arr[i] + " ");
+
+	}
+
+}
+}
---
+++ TNS/src/com/kprit/tns/arraysdemo/SortArray.java
@@ -0,0 +1,14 @@
+package com.kprit.tns.arraysdemo;
+
+import java.util.Arrays;
+
+public class SortArray {
+
+	public static void main(String[] args) {
+		int[] arr = {5, 2, 8, 7, 1};
+        Arrays.sort(arr);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+
+	}
+
+}
